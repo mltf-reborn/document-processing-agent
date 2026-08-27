49b8e52d7b9ea12717843fe8c002cbfe4bcfb735
Bagus <bagusmwicaksono@gmail.com>
2026-08-27 01:36:36 +0000

    Initializer

---
+++ src/test/java/com/bagusxmahendra/mltf/document_processing_agent/DocumentProcessingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagusxmahendra.mltf.document_processing_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentProcessingAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
